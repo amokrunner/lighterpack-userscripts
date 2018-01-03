3ea70a6b2fd3e3ff7af8786f2d5b277471ae6273
Danny Murphy <danny.murphy@amokrunner.com>
2018-01-03 10:43:40 -0800

    initial

---
+++ activeFilter.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         LighterPack Active Filter
+// @namespace    https://lighterpack.com/
+// @version      1.0
+// @description  Upgrades for lighterpack!
+// @author       amokrunner
+// @match        https://lighterpack.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $('#librarySearch').before('<div><input type="checkbox" id="librarySearchFilter" name="librarySearchFilter" /><label for="librarySearchFilter">Include Active Items</label></div>');
+    $('#library').addClass('lpHideActive');
+    $("#librarySearchFilter").change(function(){
+        if($(this).prop('checked')){
+             $("#library").removeClass('lpHideActive');
+        } else {
+             $("#library").addClass('lpHideActive');
+        }
+    });
+    $('head').append(`
+      <style>
+        .lpHideActive .lpLibraryItem:not(.lpItemNotInList){ display:none !important;}
+      </style>
+    `);
+})();
---
+++ imageHeight.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         LighterPack Image Height Fix
+// @namespace    https://lighterpack.com/
+// @version      1.0
+// @description  Upgrades for lighterpack!
+// @author       amokrunner
+// @match        https://lighterpack.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $('head').append(`
+      <style>
+        .lpImageCell{ text-align: center !important; }
+        .lpItemImage { max-height: 4em !important; }
+      </style>
+    `);
+})();
