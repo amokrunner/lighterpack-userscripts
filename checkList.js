f1da21236a8420b04b2588b69345fb30e2597f7b
Danny Murphy <danny.murphy@amokrunner.com>
2018-01-04 07:29:26 -0800

    updating checkList

--- checkList.js
+++ checkList.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LighterPack Checklist
 // @namespace    https://lighterpack.com/
-// @version      0.1
+// @version      0.2
 // @description  Upgrades for lighterpack!
 // @author       amokrunner
 // @match        https://lighterpack.com/r/*
@@ -11,28 +11,43 @@
 var lpCheckLists = {
     currentId: window.location.pathname.split(/[/ ]+/).pop(),
     data: {},
-}
+};
+
 $.getScript('https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js');
 
 (function() {
     'use strict';
 
-    
     setTimeout(function(){
     // add checkboxes
     $('.lpItem').each(function(){
         $(this).prepend('<input type="checkbox"/>');
     });
-    
+
     $('.lpItem input[type="checkbox"]').change(function(){
         lpCheckLists.data[lpCheckLists.currentId][$(this).parent().prop('id')] = $(this).prop('checked');
-        Cookies.set('lpCheckLists.data',JSON.stringify(lpCheckLists.data), { expires: 10 });
+        Cookies.set('lpCheckLists.' + lpCheckLists.currentId,JSON.stringify(lpCheckLists.data[lpCheckLists.currentId]), { expires: 10 });
+        if($(this).prop('checked')) {
+            $(this).parent().addClass('lpItemChecked');
+        } else {
+            $(this).parent().removeClass('lpItemChecked');
+        }
     });
-    
+
+    $('.lpItem').click(function(){
+        if ($(this).find('input[type="checkbox"]').prop('checked')) {
+            $(this).find('input[type="checkbox"]').prop('checked',false);
+        } else {
+            $(this).find('input[type="checkbox"]').prop('checked',true);
+        }
+        $(this).find('input[type="checkbox"]').change();
+    });
+
+
     // load data:
-    var cookie = Cookies.get('lpCheckLists.data');
+    var cookie = Cookies.get('lpCheckLists.' + lpCheckLists.currentId);
     if (cookie!==undefined) {
-        lpCheckLists.data = JSON.parse(cookie);
+        lpCheckLists.data[lpCheckLists.currentId] = JSON.parse(cookie);
         console.log(lpCheckLists.data);
     } else {
         lpCheckLists.data = {};
@@ -42,11 +57,17 @@ $.getScript('https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js');
     } else {
         Object.keys(lpCheckLists.data[lpCheckLists.currentId]).forEach(function(key) {
             $('.lpItem[id="'+key+'"] input[type="checkbox"]').prop('checked',true);
+            $('.lpItem[id="'+key+'"]').addClass('lpItemChecked');
         });
     }
-   
-    
+
+
     },250);
-    
-    
+
+    $('head').append(`
+      <style>
+        .lpItemChecked{ color:gray !important; }
+      </style>
+    `);
+
 })();
