aedc6c34eb2b111a82868dd3a1709420c2083a40
Danny Murphy <danny.murphy@amokrunner.com>
2018-01-04 10:39:27 -0800

    misc

--- checkList.js
+++ checkList.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LighterPack Checklist
 // @namespace    https://lighterpack.com/
-// @version      0.3
+// @version      0.4
 // @description  Upgrades for lighterpack!
 // @author       amokrunner
 // @match        https://lighterpack.com/r/*
@@ -13,71 +13,74 @@ var lpCheckLists = {
     data: {},
 };
 
-$.getScript('https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js');
-
 (function() {
     'use strict';
 
-    setTimeout(function(){
-        if (!lpCheckLists.currentId.length) {
-            return;
+    $.getScript(
+        'https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js',
+        function(){
+            if (!lpCheckLists.currentId.length) {
+                return;
+            }
+            // add checkboxes
+            $('.lpItem').each(function(){
+                $(this).prepend('<input type="checkbox"/>');
+            });
+
+            $('.lpItem input[type="checkbox"]').change(function(){
+                lpCheckLists.data[lpCheckLists.currentId][$(this).parent().prop('id')] = $(this).prop('checked');
+                Cookies.set('lpCheckLists.' + lpCheckLists.currentId,JSON.stringify(lpCheckLists.data[lpCheckLists.currentId]), { expires: 10 });
+                if($(this).prop('checked')) {
+                    $(this).parent().addClass('lpItemChecked');
+                } else {
+                    $(this).parent().removeClass('lpItemChecked');
+                }
+            });
+
+            $('.lpItem').click(function(){
+                if ($(this).find('input[type="checkbox"]').prop('checked')) {
+                    $(this).find('input[type="checkbox"]').prop('checked',false);
+                } else {
+                    $(this).find('input[type="checkbox"]').prop('checked',true);
+                }
+                $(this).find('input[type="checkbox"]').change();
+            });
+
+
+            // load data:
+            var cookie = Cookies.get('lpCheckLists.' + lpCheckLists.currentId);
+            if (cookie!==undefined) {
+                lpCheckLists.data[lpCheckLists.currentId] = JSON.parse(cookie);
+                console.log(lpCheckLists.data);
+            } else {
+                lpCheckLists.data = {};
+            }
+            if (lpCheckLists.data[lpCheckLists.currentId]===undefined) {
+                lpCheckLists.data[lpCheckLists.currentId] = {};
+            } else {
+                Object.keys(lpCheckLists.data[lpCheckLists.currentId]).forEach(function(key) {
+                    $('.lpItem[id="'+key+'"] input[type="checkbox"]').prop('checked',true);
+                    $('.lpItem[id="'+key+'"]').addClass('lpItemChecked');
+                });
+            }
+
+
         }
-    // add checkboxes
-    $('.lpItem').each(function(){
-        $(this).prepend('<input type="checkbox"/>');
-    });
+    );
 
-    $('.lpItem input[type="checkbox"]').change(function(){
-        lpCheckLists.data[lpCheckLists.currentId][$(this).parent().prop('id')] = $(this).prop('checked');
-        Cookies.set('lpCheckLists.' + lpCheckLists.currentId,JSON.stringify(lpCheckLists.data[lpCheckLists.currentId]), { expires: 10 });
-        if($(this).prop('checked')) {
-            $(this).parent().addClass('lpItemChecked');
-        } else {
-            $(this).parent().removeClass('lpItemChecked');
-        }
-    });
-
-    $('.lpItem').click(function(){
-        if ($(this).find('input[type="checkbox"]').prop('checked')) {
-            $(this).find('input[type="checkbox"]').prop('checked',false);
-        } else {
-            $(this).find('input[type="checkbox"]').prop('checked',true);
-        }
-        $(this).find('input[type="checkbox"]').change();
-    });
-
-
-    // load data:
-    var cookie = Cookies.get('lpCheckLists.' + lpCheckLists.currentId);
-    if (cookie!==undefined) {
-        lpCheckLists.data[lpCheckLists.currentId] = JSON.parse(cookie);
-        console.log(lpCheckLists.data);
-    } else {
-        lpCheckLists.data = {};
+    if (lpCheckLists.currentId.length) {
+        $('head').append(`
+          <style>
+            .lpItemChecked {
+                color:gray;
+                font-style: italic;
+            }
+            .lpItemChecked .lpItemImage {
+                filter: grayscale(100%);
+                opacity: 0.5;
+            }
+          </style>
+        `);
     }
-    if (lpCheckLists.data[lpCheckLists.currentId]===undefined) {
-        lpCheckLists.data[lpCheckLists.currentId] = {};
-    } else {
-        Object.keys(lpCheckLists.data[lpCheckLists.currentId]).forEach(function(key) {
-            $('.lpItem[id="'+key+'"] input[type="checkbox"]').prop('checked',true);
-            $('.lpItem[id="'+key+'"]').addClass('lpItemChecked');
-        });
-    }
-
-
-    },250);
-
-    $('head').append(`
-      <style>
-        .lpItemChecked {
-            color:gray;
-            font-style: italic;
-        }
-        .lpItemChecked .lpItemImage {
-            filter: grayscale(100%);
-            opacity: 0.5;
-        }
-      </style>
-    `);
 
 })();
