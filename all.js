90b1d3d4e75f66174a8ea5c6a443c79999b0f591
Danny Murphy <danny.murphy@amokrunner.com>
2018-01-06 11:08:29 -0800

    Adding mobile touch fix

--- all.js
+++ all.js
@@ -15,5 +15,6 @@
     $.getScript('https://rawgit.com/amokrunner/lighterpack-userscripts/master/activeFilter.js');
     $.getScript('https://rawgit.com/amokrunner/lighterpack-userscripts/master/checkList.js');
     $.getScript('https://rawgit.com/amokrunner/lighterpack-userscripts/master/imageHeight.js');
+    $.getScript('https://rawgit.com/amokrunner/lighterpack-userscripts/master/mobileTouch.js');
 
 })();
---
+++ mobileTouch.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         LighterPack Mobile Touch Fix
+// @namespace    https://lighterpack.com/
+// @version      0.1
+// @description  Upgrades for lighterpack!
+// @author       amokrunner
+// @match        https://lighterpack.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+})();
