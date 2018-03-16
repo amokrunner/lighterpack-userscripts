dc40f4853765924daf1b6282cc3f69b33cba1fa0
Danny Murphy <danny@amokrunner.com>
2018-03-16 06:05:27 -0700

    updating thumbnails

--- imageHeight.js
+++ imageHeight.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LighterPack Image Height Fix
 // @namespace    https://lighterpack.com/
-// @version      1.02
+// @version      1.1
 // @description  Upgrades for lighterpack!
 // @author       amokrunner
 // @match        https://lighterpack.com/*
@@ -17,22 +17,19 @@
       <style>
         .lpImageCell{ text-align: center !important; }
         .lpItemImage { 
-object-fit: cover; 
-width:4em; 
-height:4em; 
-}
-
-
-        @media only screen and (max-width: 720px) {
-            .lpShowImages .lpItem.lpItemHasImage {
-                min-height: 4em !important;
-                padding-left: 60px !important;
-            }
-            .lpShowImages .lpItem.lpItemHasImage .lpImageCell {
-                height: 4em !important;
-                width: 4em !important;
-            }
+            object-fit: cover; 
+            min-width:48px; 
+            max-height:48px; 
         }
       </style>
     `);
+    
+    setInterval(function(){ 
+        $('.lpItemImage').each(function(){
+            var url = $(this).attr('src');
+            if (url.indexOf('https://i.imgur.com/') >=0 ) {
+                $(this).attr('src',url.replace('s.jpg','t.jpg'));
+            }
+        });
+    },2000);
 })();
